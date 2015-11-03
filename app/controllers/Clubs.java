6a4e86c2cc51d182334b7c5e9115638c4a81d606
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-03 14:15:55 +0000

    delete club route, ui and controler

--- app/controllers/Clubs.java
+++ app/controllers/Clubs.java
@@ -3,6 +3,8 @@ package controllers;
 import java.util.List;
 
 import models.Club;
+import models.Division;
+import play.Logger;
 import play.mvc.Controller;
 
 public class Clubs extends Controller
@@ -12,4 +14,25 @@ public class Clubs extends Controller
     List<Club> clubs = Club.findAll();
     render (clubs);
   }
+  
+  public static void delete(Long id)
+  {
+    Club club = Club.findById(id);
+    if (club != null)
+    {
+      Logger.info("Trying to delete " + club.name);
+      List<Division> divisions = Division.findAll();
+      for (Division division : divisions)
+      {
+        if (division.members.contains(club))
+        {
+          division.members.remove(club);
+          division.save();
+          Logger.info ("removing club from division");
+        }
+      }
+      club.delete();
+    }
+    index();
+  } 
 }
