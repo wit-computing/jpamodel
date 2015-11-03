15f8cb714c4e32a25627ffa3a5109afdc907730f
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-03 12:55:42 +0000

    First club/player relationship + tests

--- app/models/Club.java
+++ app/models/Club.java
@@ -1,18 +1,33 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
 import play.db.jpa.Model;
 
 @Entity
 public class Club extends Model
 {
   public String name;
+  
+  @OneToMany(cascade=CascadeType.ALL)
+  public List<Player> players;
 
   public Club(String name)
   {
     this.name = name;
+    this.players = new ArrayList<Player>();
   }
   
+  public void addPlayer(Player player)
+  {
+    players.add(player);
+  }
+
   public static Club findByName(String name)
   {
     return find("name", name).first();
--- test/ClubTest.java
+++ test/ClubTest.java
@@ -7,13 +7,21 @@ import models.*;
 public class ClubTest extends UnitTest 
 {
   private Club c1, c2, c3;
+  private Player p1, p2, p3;
 
   @Before
   public void setup()
   {
+    p1 = new Player("mike");
+    p2 = new Player("jim");
+    p3 = new Player("frank");
     c1 = new Club("tramore");
     c2 = new Club("dunmore");
     c3 = new Club("fenor");
+
+    c1.addPlayer(p1);
+    c1.addPlayer(p2);
+
     c1.save();
     c2.save();
     c3.save();
@@ -46,5 +54,38 @@ public class ClubTest extends UnitTest
   {
     Club a = Club.findByName("george");
     assertNull(a);
+  }
+  
+  @Test
+  public void testPlayers()
+  {
+    Club tramore = Club.findByName("tramore");
+
+    assertEquals (2, tramore.players.size());
+
+    Player mike  = Player.findByName("mike");
+    Player jim   = Player.findByName("jim");
+    Player frank = Player.findByName("framk");
+
+    assertTrue  (tramore.players.contains(mike));
+    assertTrue  (tramore.players.contains(jim));
+    assertFalse (tramore.players.contains(frank));
+  } 
+  
+  @Test
+  public void testRemovePlayer()
+  {
+    Club tramore = Club.findByName("tramore");
+    assertEquals(2, tramore.players.size());
+
+    Player mike  = Player.findByName("mike");
+    assertTrue(tramore.players.contains(mike));
+    tramore.players.remove(mike);
+    tramore.save();
+
+    Club c = Club.findByName("tramore");
+    assertEquals(1, c.players.size());
+
+    mike.delete();
   }
 }
